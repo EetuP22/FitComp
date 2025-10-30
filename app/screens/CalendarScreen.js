83f0f9c7787d0fa0faf226caaec536a35518526e
Eetu P <sinun_emailisi@example.com>
2025-10-30 14:17:35 +0200

    Add further calendar functionality via workout log creation and navigation to programScreen

--- app/screens/CalendarScreen.js
+++ app/screens/CalendarScreen.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import CustAppBar from '../components/CustAppBar';
 import { Calendar } from 'react-native-calendars';
+import { TextInput, Button, Card } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 
 export default function CalendarScreen() {
     const [selectedDate, setSelectedDate] = React.useState('');
+    const [workoutName, setWorkoutName] = useState('');
+    const [workouts, setWorkouts] = useState([]);
+    const navigation = useNavigation();
+
+
+    const addWorkout = () => {
+        if (!selectedDate || !workoutName.trim() === '') return;
+        const newWorkout = { date: selectedDate, name: workoutName.trim() };
+        setWorkouts((prev) => [...prev, newWorkout]);
+        setWorkoutName('');
+    }
+
+    const goToProgram = (workout) => {
+        navigation.navigate('Programs', { workout });
+    };
 
 
   return (
     <View style={styles.container}>
       <CustAppBar title="Treenikalenteri" />
+
       <Calendar
         onDayPress={(day) => setSelectedDate(day.dateString)}
         markedDates={{
@@ -24,9 +42,45 @@ export default function CalendarScreen() {
 
       <View style={styles.content}>
         {selectedDate ? (
+            <>
             <Text style={styles.text}>Valittu päivä: {selectedDate}</Text>
+
+            <TextInput
+                label="Treenin nimi"
+                value={workoutName}
+                onChangeText={setWorkoutName}
+                mode='outlines'
+                style={styles.input}
+                />
+
+            <Button mode="contained" onPress={addWorkout} style={styles.button}>
+                Lisää treeni
+            </Button>
+
+             <FlatList
+              data={workouts.filter((w) => w.date === selectedDate)}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
+                <Card style={styles.card}>
+                  <Card.Title
+                    title={item.name}
+                    subtitle={`Päivä: ${item.date}`}
+                    right={(props) => (
+                      <Button
+                        onPress={() => goToProgram(item)}
+                        compact
+                        mode="text"
+                      >
+                        Avaa
+                      </Button>
+                    )}
+                  />
+                </Card>
+              )}
+            />
+          </>
         ) : (
-            <Text style={styles.text}>Valitse päivä kalenterista 📅</Text>
+          <Text style={styles.text}>Valitse päivä kalenterista 📅</Text>
         )}
       </View>
     </View>
@@ -34,7 +88,10 @@ export default function CalendarScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {flex: 1, backgroundColor: '#fff' },
-    content: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-    text: { fontSize: 18, color: '#333'},
+  container: { flex: 1, backgroundColor: '#fff' },
+  content: { flex: 1, padding: 16 },
+  text: { fontSize: 18, color: '#333', marginBottom: 12 },
+  input: { marginBottom: 10 },
+  button: { marginBottom: 16 },
+  card: { marginVertical: 6 },
 });
